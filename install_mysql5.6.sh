19e71567217ac0dd43713b3950183fe2d7fbbb01
JeffXue <contactmexkj@163.com>
2015-12-21 14:15:58 +0800

    fix mysql install script

--- install_mysql5.6.sh
+++ install_mysql5.6.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+time=`date +%Y%m%d%H%M`
+
+ftp_server=192.168.1.113
+ftp_user=admin
+ftp_password=123456
+
+ftp_response=`ftp -n -v << EOF 2>&1
+open $ftp_server
+user $ftp_user $ftp_password
+get mysql-5.6.16-debian6.0-x86_64.deb
+bye
+EOF`
 
 dpkg -i mysql-5.6.16-debian6.0-x86_64.deb
 
@@ -8,7 +20,11 @@ apt-get install mysql-client -y
 
 ln -s /opt/mysql/server-5.6/ /usr/local/mysql
 
-cd /opt/mysql/server-5.6
+mv /etc/mysql/my.cnf /etc/mysql/my.cnf_$time
+
+cp my.cnf /usr/local/mysql/
+
+cd /usr/local/mysql
 
 groupadd mysql
 
@@ -18,13 +34,13 @@ chown -R mysql .
 
 chgrp -R mysql .
 
-mv /etc/mysql/my.cnf /etc/mysql/my.cnf_backup
-
 scripts/mysql_install_db --user=mysql
 
 chown -R root .
 
 chown -R mysql data
 
-bin/mysqld_safe --user=mysql &
+./bin/mysqld_safe --user=mysql &
+
+echo "please run the mysql_init.sql!"
 
