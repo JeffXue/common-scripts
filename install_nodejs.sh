08b268f28bd5c60d4918a96627907fce9fea4777
JeffXue <contactmexkj@163.com>
2016-03-22 21:24:45 +0800

    fix

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -4,4 +4,4 @@ chmod +x install.sh
 ./install.sh
 echo "reopen terminal and run 'nvm install 4.3.2' to install nodejs"
 rm -rf install.sh
-echo "add PATH=/youHomeDir/.nvm/versions/node/v4.3.2/bin:\$PATH to ~/.profile"
+#echo "add PATH=/youHomeDir/.nvm/versions/node/v4.3.2/bin:\$PATH to ~/.profile"
