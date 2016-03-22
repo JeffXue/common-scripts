eea6bc14e17afe3e4dd62922da330dfbf21edf6a
JeffXue <contactmexkj@163.com>
2016-03-22 11:00:45 +0800

    fixed

--- install_nginx1.8.sh
+++ install_nginx1.8.sh
@@ -15,5 +15,5 @@ cd ..
 rm -rf temp
 
 cd /usr/local/nginx-1.8.1
-./configure --prefix=/usr/local/nginx --with-http_ssl_module --with-http_stub_status_module --with-http_gzip_static_module --with-pcre=/usr/local/pcre-8.10 --with-openssl=/usr/local/openssl-1.0.1p --with-zlib=/usr/local/zlib-1.2.7
+./configure --prefix=/usr/local/nginx --with-http_ssl_module --with-http_stub_status_module --with-http_gzip_static_module --with-pcre=/usr/local/pcre-8.10 --with-openssl=/usr/local/openssl-1.0.1p --with-zlib=/usr/local/zlib-1.2.7 --with-http_realip_module
 make && make install
