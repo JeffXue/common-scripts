1294082799708246adb08e325ed7f4ed89c1aeb7
JeffXue <contactmexkj@163.com>
2016-04-15 16:15:36 +0800

    update to 2.8.24

--- install_redis2.8.sh
+++ install_redis2.8.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 mkdir temp
 cd temp
-wget --no-check-certificate https://raw.githubusercontent.com/JeffXue/common_packages/master/redis-2.8.17.tar.gz
+wget --no-check-certificate https://raw.githubusercontent.com/JeffXue/common_packages/master/redis-2.8.24.tar.gz
 
 tar xvzf redis-2.8.17.tar.gz -C /usr/local/
 
 cd ..
 rm -rf temp
 
-cd /usr/local/redis-2.8.17
+cd /usr/local/redis-2.8.24
 make
 
 mkdir /usr/local/redis
