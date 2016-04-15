bc89a4157c3c3d0d83fd4098fd4c61408dc3d9f2
JeffXue <contactmexkj@163.com>
2016-04-15 16:34:59 +0800

    fixed

--- install_redis2.8.sh
+++ install_redis2.8.sh
@@ -3,7 +3,7 @@ mkdir temp
 cd temp
 wget --no-check-certificate https://raw.githubusercontent.com/JeffXue/common_packages/master/redis-2.8.24.tar.gz
 
-tar xvzf redis-2.8.17.tar.gz -C /usr/local/
+tar xvzf redis-2.8.24.tar.gz -C /usr/local/
 
 cd ..
 rm -rf temp
@@ -12,6 +12,6 @@ cd /usr/local/redis-2.8.24
 make
 
 mkdir /usr/local/redis
-cp redis.conf /usr/local/redis
+cp redis.conf sentinel.conf /usr/local/redis
 cd src
-cp redis-benchmark redis-server redis-cli  /usr/local/redis
+cp redis-benchmark redis-server redis-cli redis-sentinel /usr/local/redis
