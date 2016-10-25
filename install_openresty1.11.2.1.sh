c2bf57de9e1bb41dab18bbb7764d2ae125821627
JeffXue <contactmexkj@163.com>
2016-10-25 15:39:17 +0800

    update

---
+++ install_openresty1.11.2.1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get install libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make build-essential -y
+
+mkdir temp
+cd temp
+wget --no-check-certificate https://raw.githubusercontent.com/JeffXue/common_packages/master/openresty-1.11.2.1.tar.gz
+
+tar xvzf openresty-1.11.2.1.tar.gz -C /usr/local/
+
+cd ..
+rm -rf temp
+
+cd /usr/local/openresty-1.11.2.1
+make
+make install
+
