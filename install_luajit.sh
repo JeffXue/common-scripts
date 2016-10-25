50da069ef741dec270e40bd7b678640aedf124a6
root <root@debian>
2016-10-25 09:54:41 +0800

    update script

---
+++ install_luajit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir temp
+cd temp
+wget --no-check-certificate https://raw.githubusercontent.com/JeffXue/common_packages/master/LuaJIT-2.1.0-beta2.tar.gz
+
+tar xvzf LuaJIT-2.1.0-beta2.tar.gz -C /usr/local/
+
+cd ..
+rm -rf temp
+
+cd /usr/local/LuaJIT-2.1.0-beta2
+make
+make install
+
+ln -sf /usr/local/bin/luajit-2.1.0-beta2 /usr/local/bin/luajit
